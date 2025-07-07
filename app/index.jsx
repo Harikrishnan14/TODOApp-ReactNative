57933081907a27c0b403166304ac1dcf3fa68d30
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-07-07 16:24:30 +0530

    Update: addTodo function

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { data } from '@/data/todos'
 
 export default function Index() {
+  const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id))
+  const [text, setText] = useState('')
+
+  const addTodo = () => {
+    if (text.trim()) {
+      const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+      setTodos([{ id: newId, title: text, completed: false }, ...todos])
+      setText('')
+    }
+  }
+
   return (
     <View
       style={{
