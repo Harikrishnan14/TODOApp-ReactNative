51b7d2d50019cdc6f82636ba31628b8953922ce9
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-07-07 16:27:51 +0530

    Update: removeTodo function

--- app/index.jsx
+++ app/index.jsx
@@ -19,6 +19,10 @@ export default function Index() {
     setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
   }
 
+  const removeTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
   return (
     <View
       style={{
